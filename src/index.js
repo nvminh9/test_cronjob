a540f084148888cc15afe7931c55ee0a3ad43b85
minhngo <minhcorn.2@gmail.com>
2025-12-14 14:03:14 +0700

    [Update] config cors, static file...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import express from 'express';
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3800;
 
+// --- CONFIG ---
+// CORS
+app.use(cors());
+// req.body
+app.use(express.json()); // For json
+app.use(express.urlencoded({ extended: true })); // For form data
+// static file
+app.use(express.static(path.join(__dirname, '../assets'))); // Không cần Auth
+
+// Test
 app.get('/', (req, res) => {
     res.send('Hello from test_cronjob');
 });
